4f72015073cd5209d475148b8da7b20dd0982ec9
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-02 15:59:27 +0300

    added Shop, Items, GenreFilters components

---
+++ src/components/Shop/GenreFilters.jsx
@@ -0,0 +1,3 @@
+const GenreFilters = () => {};
+
+export default GenreFilters;
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,3 +1,44 @@
-const Shop = () => <div>shop</div>;
+import "../css/Shop.css";
+import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import GenreFilters from "../components/Shop/GenreFilters";
+import Items from "../components/Shop/Items";
+import { getGames } from "../utils/games";
+
+export const loader = async () => {
+  const games = await getGames();
+  return games;
+};
+
+const Shop = () => {
+  const games = useLoaderData();
+  const [page, setPage] = useState(1);
+  const [filter, setFilter] = useState(null);
+
+  const handlePageClick = (e) => {
+    const clickedPage = Number(e.target.dataset.page);
+    setPage(clickedPage);
+  };
+
+  return (
+    <main id="shop">
+      <div>
+        <GenreFilters games={games} setFilter={setFilter} />
+        <Items page={page} filter={filter} />
+        <div className="pages">
+          <button type="button" data-page="1" onClick={handlePageClick}>
+            1
+          </button>
+          <button type="button" data-page="2" onClick={handlePageClick}>
+            2
+          </button>
+          <button type="button" data-page="3" onClick={handlePageClick}>
+            3
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Shop;
