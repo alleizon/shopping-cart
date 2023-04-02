c8f69db230cb7abcadb61eac11b899893a2c9c98
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-02 22:01:03 +0300

    GenreFilters component rendering based on window width

--- src/components/Shop/GenreFilters.jsx
+++ src/components/Shop/GenreFilters.jsx
@@ -1,3 +1,44 @@
-const GenreFilters = () => {};
+// index can be used as key because genre values never change
+/* eslint-disable react/no-array-index-key */
+
+import { getGenres } from "../../utils/games";
+
+const GenreFilters = ({ filter, hideFilters, setFilter }) => {
+  const genres = getGenres();
+
+  return (
+    <div id="filters">
+      {hideFilters ? (
+        <select
+          value={filter || "Filters"}
+          onChange={(e) => {
+            if (!genres.includes(e.target.value)) {
+              setFilter(null);
+              return;
+            }
+            setFilter(e.target.value);
+          }}
+        >
+          <option>Filters</option>
+          {genres.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <ul>
+          {genres.map((genre, index) => (
+            <li key={index} className={filter === genre ? "active" : ""}>
+              <button type="button" onClick={() => setFilter(genre)}>
+                {genre}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default GenreFilters;
