9226926a04fc73f1af681aaadc557e68715a109e
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-02 14:48:20 +0300

    active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,23 +1,35 @@
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "../css/Root.css";
 
 const Root = () => (
-    <>
-      <nav>
-        <Link id="home-link" to="/">
-          Home
-        </Link>
-        <Link id="shop-link" to="shop">
-          Shop
-        </Link>
-        <Link id="cart-link" to="cart">
-          <FontAwesomeIcon icon={faCartShopping} />
-        </Link>
-      </nav>
-      <Outlet />
-    </>
-  );
+  <>
+    <nav>
+      <NavLink
+        id="home-link"
+        className={({ isActive }) => (isActive ? "active" : "")}
+        to="/"
+      >
+        Home
+      </NavLink>
+      <NavLink
+        id="shop-link"
+        className={({ isActive }) => (isActive ? "active" : "")}
+        to="shop"
+      >
+        Shop
+      </NavLink>
+      <NavLink
+        id="cart-link"
+        className={({ isActive }) => (isActive ? "active" : "")}
+        to="cart"
+      >
+        <FontAwesomeIcon icon={faCartShopping} />
+      </NavLink>
+    </nav>
+    <Outlet />
+  </>
+);
 
 export default Root;
