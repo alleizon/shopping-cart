c3e91380486a4c7f8389551c4a7786920a56fbc8
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-09 17:30:33 +0300

    extracted AddToCart component

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,39 @@
+import { useOutletContext } from "react-router-dom";
+
+const AddToCart = ({ game }) => {
+  const [cartItems, setCartItems] = useOutletContext();
+
+  const handleAddToCart = (action) => {
+    const cartCopy = cartItems.slice();
+    const gameCopy = game;
+    if (action === "add") {
+      cartCopy.push(game);
+      gameCopy.addedToCart = true;
+    } else {
+      const index = cartCopy.findIndex((cartGame) => cartGame === gameCopy);
+      cartCopy.splice(index, 1);
+      gameCopy.addedToCart = false;
+    }
+    setCartItems(cartCopy);
+  };
+
+  return game.addedToCart ? (
+    <button
+      className="remove-from-cart"
+      type="button"
+      onClick={() => handleAddToCart("remove")}
+    >
+      Remove
+    </button>
+  ) : (
+    <button
+      className="add-to-cart"
+      type="button"
+      onClick={() => handleAddToCart("add")}
+    >
+      Add to cart
+    </button>
+  );
+};
+
+export default AddToCart;
--- src/components/Shop/GameCard.jsx
+++ src/components/Shop/GameCard.jsx
@@ -1,58 +1,25 @@
-import { Link, useOutletContext } from "react-router-dom";
+import { Link } from "react-router-dom";
+import AddToCart from "../AddToCart";
 
-const GameCard = ({ game }) => {
-  const [cartItems, setCartItems] = useOutletContext();
-
-  const handleAddToCart = (action) => {
-    const cartCopy = cartItems.slice();
-    const gameCopy = game;
-    if (action === "add") {
-      cartCopy.push(game);
-      gameCopy.addedToCart = true;
-    } else {
-      const index = cartCopy.findIndex((cartGame) => cartGame === gameCopy);
-      cartCopy.splice(index, 1);
-      gameCopy.addedToCart = false;
-    }
-    setCartItems(cartCopy);
-  };
-
-  return (
-    <div className="item-container">
-      <div className="card-img-wrapper">
-        <p className="card-hover">{game.name}</p>
-        <img
-          src={game.backgroundImage}
-          alt={game.name}
-          onLoad={(e) => {
-            e.target.style.opacity = "1";
-          }}
-        />
-      </div>
-      <div className="card-buttons">
-        <Link to={`/product/${game.slug}`} className="dark-button">
-          Read More
-        </Link>
-        {game.addedToCart ? (
-          <button
-            className="remove-from-cart"
-            type="button"
-            onClick={() => handleAddToCart("remove")}
-          >
-            Remove
-          </button>
-        ) : (
-          <button
-            className="add-to-cart"
-            type="button"
-            onClick={() => handleAddToCart("add")}
-          >
-            Add to cart
-          </button>
-        )}
-      </div>
+const GameCard = ({ game }) => (
+  <div className="item-container">
+    <div className="card-img-wrapper">
+      <p className="card-hover">{game.name}</p>
+      <img
+        src={game.backgroundImage}
+        alt={game.name}
+        onLoad={(e) => {
+          e.target.style.opacity = "1";
+        }}
+      />
     </div>
-  );
-};
+    <div className="card-buttons">
+      <Link to={`/product/${game.slug}`} className="dark-button">
+        Read More
+      </Link>
+      <AddToCart game={game} />
+    </div>
+  </div>
+);
 
 export default GameCard;
