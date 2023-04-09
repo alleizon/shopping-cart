486b2fff6df3c2fce237995f63ac79ab97f08dad
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-09 17:01:56 +0300

    added swapping between bgImg and bgImgAdditional

--- src/components/Product/ImageCarousel.jsx
+++ src/components/Product/ImageCarousel.jsx
@@ -1,5 +1,50 @@
-const ImageCarousel = () => {
-  return <div id="img-carousel"></div>;
+import { useState } from "react";
+
+const ImageCarousel = ({ game }) => {
+  const [current, setCurrent] = useState(game.backgroundImage);
+
+  const handleClick = (e) => {
+    const btnIdx = Number(e.currentTarget.dataset.btnIdx);
+    if (btnIdx === 1) setCurrent(game.backgroundImageAdditional);
+    else setCurrent(game.backgroundImage);
+  };
+
+  return (
+    <div id="img-carousel">
+      <div>
+        <img
+          className={current === game.backgroundImage ? "active" : ""}
+          src={game.backgroundImage}
+          alt={game.name}
+          data-img-idx="0"
+        />
+        <img
+          className={current === game.backgroundImageAdditional ? "active" : ""}
+          src={game.backgroundImageAdditional}
+          alt={game.name}
+          data-img-idx="1"
+        />
+        <div className="carousel-panel">
+          <button
+            className={current === game.backgroundImage ? "active" : ""}
+            type="button"
+            data-btn-idx="0"
+            aria-label="image-index"
+            onClick={handleClick}
+          />
+          <button
+            className={
+              current === game.backgroundImageAdditional ? "active" : ""
+            }
+            type="button"
+            data-btn-idx="1"
+            aria-label="image-index"
+            onClick={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageCarousel;
