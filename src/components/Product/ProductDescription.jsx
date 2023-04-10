6d88e3f1d0ef62a249ecc88036f5910723ead386
alleizon <112495304+alleizon@users.noreply.github.com>
2023-04-10 13:52:50 +0300

    added Product description

--- src/components/Product/ProductDescription.jsx
+++ src/components/Product/ProductDescription.jsx
@@ -1,5 +1,18 @@
-const ProductDescription = () => {
-  return <div id="product-description"></div>;
+import { useEffect, useRef } from "react";
+
+const ProductDescription = ({ game }) => {
+  const descRef = useRef(null);
+
+  useEffect(() => {
+    descRef.current.innerHTML = game.description;
+  }, []);
+
+  return (
+    <div id="product-description">
+      <h1>Description</h1>
+      <p ref={descRef} />
+    </div>
+  );
 };
 
 export default ProductDescription;
